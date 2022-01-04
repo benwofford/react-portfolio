47347e81c46f710ad3d0f304ee38d359cf9433cb
BenWofford <woffordbt@gmail.com>
2022-01-04 16:51:22 -0500

    Navbar styling changes

--- src/components/Navbar/Menuitems.jsx
+++ src/components/Navbar/Menuitems.jsx
@@ -24,9 +24,4 @@ export const MenuItems = [
     url: "#",
     cName: "nav-links",
   },
-  {
-    title: "Mobile",
-    url: "#",
-    cName: "nav-links-mobile",
-  },
 ];
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,22 @@ import { MenuItems } from "./Menuitems";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  state = { clicked: false };
+
+  handleClick = () => this.setState({ clicked: !this.state.clicked });
+
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">
           Welcome!<i className="fab fa-react"></i>
         </h1>
-        <div className="menu-icon">--</div>
-        <ul>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
