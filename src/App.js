770af55f3379003c107c1d11f275fd68e7237e20
BenWofford <woffordbt@gmail.com>
2022-01-11 15:30:47 -0500

    changed color scheme & added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./css/App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="Footer">
+    <p className="FooterContent">&copy; 2022</p>
+  </div>
+);
+
+export default Footer;
