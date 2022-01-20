2bdad3a4f35942fc4f198c6eaecce4debd36e68b
BenWofford <woffordbt@gmail.com>
2022-01-20 13:28:59 -0500

    router changes for deployment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contactform" element={<ContactForm />} />
-      </Routes>
-    </Router>
+    <BrowserRouter basename="{process.env.PUBLIC_URL}">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contactform" element={<ContactForm />} />
+        </Routes>
+      </Router>
+    </BrowserRouter>
   );
 }
 
