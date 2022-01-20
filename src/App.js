29c07a0d846e4512d071e41c4b0f6049dd14bc56
BenWofford <woffordbt@gmail.com>
2022-01-20 13:59:30 -0500

    deleted BrowserRouter link in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
-    <BrowserRouter basename="{process.env.PUBLIC_URL}">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contactform" element={<ContactForm />} />
-        </Routes>
-      </Router>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contactform" element={<ContactForm />} />
+      </Routes>
+    </Router>
   );
 }
 
